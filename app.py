0b3070d17f722ba70f341db61afb830b8f9e2139
tupham <tuan.minh.pham@cern.ch>
2021-10-02 22:35:20 +0700

    dummy change

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
